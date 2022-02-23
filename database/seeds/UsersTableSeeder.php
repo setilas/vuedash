f830af6304aa95c0cc1d0d8a39768063afb10611
Sathishsk1 <sathishkumar12128@gmail.com>
2022-02-23 15:08:21 +0530

    task
    changed laraspace@2020
    created a new user id in dashboard

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,25 +11,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'email' => 'admin@laraspace.in',
-            'name' => 'Jane Doe',
-            'role' => 'admin',
-            'password' => bcrypt('admin@123')
+        // User::create([
+        //     'email' => 'admin@laraspace.in',
+        //     'name' => 'Jane Doe',
+        //     'role' => 'admin',
+        //     'password' => bcrypt('admin@123')
+        // ]);
+
+        // User::create([
+        //     'email' => 'shane@laraspace.in',
+        //     'name' => 'Shane White',
+        //     'role' => 'user',
+        //     'password' => bcrypt('hank@123')
+        // ]);
+
+        // User::create([
+        //     'email' => 'adam@laraspace.in',
+        //     'name' => 'Adam David',
+        //     'role' => 'user',
+        //     'password' => bcrypt('jesse@123')
+        // ]);
+
+
+ User::create([
+            'email' => 'satishkumar@cgbod.in',
+            'name' => 'satishkumar',
+            'role' => 'user',
+            'password' => bcrypt('satish@123')
         ]);
 
-        User::create([
-            'email' => 'shane@laraspace.in',
-            'name' => 'Shane White',
-            'role' => 'user',
-            'password' => bcrypt('hank@123')
-        ]);
 
-        User::create([
-            'email' => 'adam@laraspace.in',
-            'name' => 'Adam David',
-            'role' => 'user',
-            'password' => bcrypt('jesse@123')
-        ]);
     }
 }
